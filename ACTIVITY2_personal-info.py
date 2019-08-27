47783ef0da90572763d523ac46f655319bbde958
castroeugene <54564686+castroeugene@users.noreply.github.com>
2019-08-26 19:24:17 -0700

    Add files via upload

---
+++ ACTIVITY2_personal-info.py
@@ -0,0 +1,7 @@
+space=" "
+fname = input("First name: ")
+lname = input("Last name: ")
+location = input("Location: ")
+age = input("Age: ")
+
+print("Hello " + fname + space + lname + space + "from " + location + space + "you are " + age + space + "years old")
